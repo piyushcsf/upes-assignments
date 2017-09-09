d857ab6159d15723f12e6bb6c60f1ab8e12a795f
piyushcsf <piyushbca7@gmail.com>
2017-09-10 01:28:34 +0530

    Update prog4.py

--- Piyush Tyagi/prog4.py
+++ Piyush Tyagi/prog4.py
@@ -1,21 +1,24 @@
-lists = []
-strinf = []
-numbers = []
-test_case=int(input("Enter the number of test cases"))
-for test in range(test_case):
-    N = int(input("N:\t"))
-    lists=input("List:\t")
-    lists=lists.split( )
-    numbers=input("A B C:\t")
-    numbers=numbers.split( )
-    string=input("String:\t")
-    for i in range(N):
-        if string[i]=='R':
-            lists[i:] = reversed(lists[i:])
-        elif string[i]=='A':
-            lists[i:] = [int(x)+int(numbers[0]) for x in lists[i:]]
-        elif string[i]=='M':
-            lists[i:] = [int(x)*int(numbers[1]) for x in lists[i:]]
-
-    lists[i:] = [x%int(numbers[2]) for x in lists[i:]]
-    print(lists)
+import functools
+from fractions import gcd
+total_no = int(input("test cases"))
+for total_test_case in range(total_no):
+        data = []
+        datanew = []
+        gcd_value = []
+        product = 1
+        total_values = int(input("total numbers"))
+        data=input("numbers:\t")
+        data=data.split( )
+        for i in range(total_values):
+                data[i]=int(data[i])
+        for i in range(1<<len(data)):
+                for j in range(len(data)):
+                        if (i&(1<<j)) > 0:
+                                datanew.append(data[j])
+                if not datanew:
+                        continue
+                gcd_value.append(functools.reduce(gcd,datanew))
+                datanew = []
+        for x in gcd_value:
+                product=int(product)* int(x)
+        print (product%10000000007)
